ebf86cba11cc178d53357c67125f529f245bdb6e
Kel Ming <kkgm013@gmail.com>
2023-08-10 16:52:08 +0100

    JavaFX running but not fully completed

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,28 @@
 import controller.SimController;
+import javafx.application.Application;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
 import model.Sim;
 import view.CLI;
+import view.javafx.JFXView;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class Main {
+public class Main extends Application {
 
     public static void main(String[] args) {
-        //Test
-//        Sim sim = new Sim("447518497796","Personal SIM",  000, "United Kingdom", "O2", "Micro", 12.32, true, true, "", LocalDateTime.now());
-//        System.out.println(sim);
-
         // Real Implementation
-        SimController controller = new SimController();
+        SimController controllerCLI = new SimController();
         CLI cli = new CLI();
-        controller.addSIM(new Sim("447518497796","Personal SIM",  "United Kingdom", "O2", "Micro", "0000",12.32, true, "Micro",true, "", LocalDateTime.now()));
-        controller.addSIM(new Sim("639157871608","Personal SIM",  "Philippines", "Globe", "Standard", "0000", 00.00, true, "Standard", false, "Deactivated in 2015", LocalDateTime.now()));
-        controller.deactivateSIM("639157871608");
+        controllerCLI.addSIM(new Sim("447518497796","Personal SIM",  "United Kingdom", "O2", "Micro", "0000",12.32, true, "Micro",true, "", LocalDateTime.now()));
+        controllerCLI.addSIM(new Sim("639157871608","Personal SIM",  "Philippines", "Globe", "Standard", "0000", 00.00, true, "Standard", false, "Deactivated in 2015", LocalDateTime.now()));
+        controllerCLI.deactivateSIM("639157871608");
         System.out.println("===================================");
 
-        List<Sim> matching1 = controller.getSIMCardsByCarrier("Globe");
+        List<Sim> matching1 = controllerCLI.getSIMCardsByCarrier("Globe");
         if(!matching1.isEmpty()){
             for(Sim sim : matching1){
                 System.out.println("Match Found: ");
@@ -32,7 +32,7 @@ public class Main {
             System.out.println("No Carrier Name listed in database");
         }
         System.out.println("===================================");
-        List<Sim> matching2 = controller.searchSIMByNumber("16");
+        List<Sim> matching2 = controllerCLI.searchSIMByNumber("16");
         if(!matching2.isEmpty()){
             System.out.println("Match(es) Found! ");
             for(Sim sim : matching2){
@@ -42,13 +42,35 @@ public class Main {
             System.out.println("No Number found in Database");
         }
         System.out.println("\n===================================");
-        controller.saveToFile();
+        controllerCLI.saveToFile();
         System.out.println("\n===================================");
-        controller.loadFromFile();
+        controllerCLI.loadFromFile();
         System.out.println("Database Includes:");
-        System.out.println(controller.getAllSIMs());
+        System.out.println(controllerCLI.getAllSIMs());
         System.out.println("\n===================================");
+        // GUI Test
+        launch(args);
+    }
 
-        //
+    @Override
+    public void start(Stage primaryStage) {
+        SimController guiController = new SimController();
+        JFXView gui = new JFXView(guiController);
+
+        primaryStage.setTitle("SIM Database");
+        primaryStage.setScene(gui.getScene());
+        // Load icon image from the resource folder
+        Image iconImage = new Image(getClass().getResourceAsStream("/graphics/duo-sim-cards-64.png"));
+        // Set the application icon
+        primaryStage.getIcons().add(iconImage);
+        // Set the program name for macOS
+        System.setProperty("apple.awt.application.name", "SIM Database");
+        System.out.println(System.getProperties());
+        // Set the program name for Windows
+        primaryStage.setTitle("SIM Database");
+        primaryStage.getIcons().add(new Image("/resources/graphics/duo-sim-cards-64.png"));
+        primaryStage.setMinWidth(450);
+        primaryStage.setMinHeight(420);
+        primaryStage.show();
     }
 }
--- src/view/javafx/JFXView.java
+++ src/view/javafx/JFXView.java
@@ -1,26 +1,124 @@
 package view.javafx;
+import controller.SimController;
 import javafx.application.Application;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.CheckBoxTableCell;
+import javafx.scene.image.Image;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import model.Sim;
+
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.VBox;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Optional;
+import javafx.util.Callback;
+
+import javax.swing.text.Element;
+import javax.swing.text.html.ImageView;
 
 
-public class JFXView extends Application{
+public class JFXView{
+    private SimController controller;
+    final private Scene scene;
+    final int gap = 6;
+    public JFXView(SimController controller){
+        this.controller = controller;
+//        VBox root = new VBox(6 * gap);
+        Pane paneRoot = new StackPane();
+        paneRoot.setPadding(new Insets(gap,gap,gap,gap));
+        Text titleText = new Text("SIM Database");
+        titleText.setFont(Font.font("Tahoma", FontWeight.BOLD, 20));
+//        ImageView logo = new ImageView((Element) new Image(getClass().getResourceAsStream("/graphics/icon.png")));
+        paneRoot.getChildren().addAll(titleText);
+//
+//        Button actButton = new Button("Press here");
+//        actButton.setOnAction((event) -> {
+//            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+//            alert.setHeaderText("Test");
+//            alert.setContentText("Hello world");
+//            ButtonType exit = new ButtonType("Exit");
+//            alert.getButtonTypes().setAll(exit);
+//            Optional result = alert.showAndWait();
+//            if(result.get() == exit){
+//                System.exit(0);
+//            }
+//        });
+//
+//        // Add all UI Elements to the Scene in an order
+//        paneRoot.getChildren().addAll(actButton);
+//        // Set the scene size
+//        this.scene = new Scene(root,
+//                (4*gap) * gap,
+//                (3*gap)* gap
+//        );
 
-    @Override
-    public void start(Stage primStage) {
-        Pane root = new StackPane();
-        Label label = new Label("Hello world");
-        Button actButton = new Button("Press here");
-        actButton.setOnAction((event) -> label.setVisible(!label.isVisible()));
-        root.getChildren().addAll(label,actButton);
+        TableView tableView = new TableView();
 
-        primStage.setTitle("Application Name Here");
-        primStage.setScene(new Scene(root,1080,1080));
-        primStage.show();
+        TableColumn<Sim, String> column1 = new TableColumn<>("SIM Name");
+        column1.setCellValueFactory(new PropertyValueFactory<>("simName"));
+        TableColumn<Sim, String> column2 = new TableColumn<>("SIM Country");
+        column2.setCellValueFactory(new PropertyValueFactory<>("simCountry"));
+//        TableColumn<Sim, Boolean> column3 = new TableColumn<>("SIM Roaming");
+//        column3.setCellValueFactory(new PropertyValueFactory<>(("isRoaming")));
+//        column3.setCellFactory(CheckBoxTableCell.forTableColumn(column3));
+//        TableColumn<Sim, Boolean> column4 = new TableColumn<>("SIM Status");
+//        column4.setCellValueFactory(new PropertyValueFactory<>("isActive"));
+//        column4.setCellFactory(CheckBoxTableCell.forTableColumn(column4));
+        TableColumn<Sim, String> column5 = new TableColumn<>("SIM Credit");
+        column5.setCellValueFactory(new PropertyValueFactory<>("simCredit"));
+        TableColumn<Sim, LocalDateTime> column6 = new TableColumn<>("Last Updated");
+        column6.setCellValueFactory(new PropertyValueFactory<>("lastUpdated"));
+        // Create the Date Time formatter
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM-dd-yyyy @ HH:mm");
+        column6.setCellFactory(new Callback<>() { // Customize the Last Updated Cell Value
+            @Override
+            public TableCell<Sim, LocalDateTime> call(TableColumn<Sim, LocalDateTime> simLocalDateTimeTableColumn) {
+                return new TableCell<>() {
+                    @Override
+                    protected void updateItem(LocalDateTime item, boolean empty) {
+                        super.updateItem(item, empty);
+                        if (empty || item == null) {
+                            setText(null);
+                        } else {
+                            setText(formatter.format(item));
+                        }
+                    }
+                };
+            }
+        });
 
+        tableView.getColumns().addAll(column1,column2, column5, column6);
+
+        tableView.getItems().add(new Sim("447518497796","Personal SIM",  "United Kingdom", "O2", "Micro", "0000",12.32, true, "Micro",true, "", LocalDateTime.now()));
+        tableView.getItems().add(new Sim("639157871608","Personal SIM",  "Philippines", "Globe", "Standard", "0000", 00.00, true, "Standard", false, "Deactivated in 2015", LocalDateTime.now()));
+
+        TableView.TableViewSelectionModel<Sim> selectionModel = tableView.getSelectionModel();
+        selectionModel.setSelectionMode(SelectionMode.SINGLE);
+        selectionModel.setSelectionMode(SelectionMode.MULTIPLE);
+        // Create an Observation List
+        ObservableList<Sim> selectedItems = selectionModel.getSelectedItems();
+        // Print out Selected/Highlighted
+        selectedItems.addListener((ListChangeListener<Sim>) change -> System.out.println("Selection changed: " + change.getList()));
+
+        VBox vbox = new VBox(paneRoot,tableView);
+        this.scene = new Scene(vbox,
+            (4*gap) * gap,
+            (3*gap)* gap
+       );
+    }
+
+    public Scene getScene() {
+        return scene;
     }
 }
